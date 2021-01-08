353332260a717ca16d3f43b33f6b7994f3c076f4
sjm3524 <sjm3524@rit.edu>
2021-01-08 15:48:29 -0500

    fixed cloud function

--- functions/index.js
+++ functions/index.js
@@ -10,47 +10,63 @@ admin.initializeApp();
 //   response.send("Hello from Firebase!");
 // });
 
-exports.addPoint = functions.database.ref('games/').onCreate( async(snapshot, context) => {
-	
-	await admin.database().ref('games/').limitToLast(1).once('value', snap => {
+/*await admin.database().ref('games/').limitToLast(1).once('value', snap => {
 		
 		snap.forEach((childSnapshot) => {
-
 			var value = childSnapshot.val();
 			console.log(value);
 			gameId=value.id;
 			console.log(value.id);
-			if (value.isActive){
-				console.log("active");
-				admin.database().ref('games/' + gameId+"/points/").limitToLast(1).on('child_added', newSnap => {
+		}
+});*/
+
+
+
+exports.addPoint = functions.database.ref('games/{gameId}/points/{pointId}').onCreate( async (snapshot, context) => {
+	
+			var point = snapshot.val();
+
+			
 										
 	
-					point =newSnap.val();
-					console.log(point);
-					console.log("adding point")
-					admin.database().ref('users/' + point.id).update({points: admin.database.ServerValue.increment(1)});
 					
-					if(point.id === value.player1 || point.id === value.player2){
-						admin.database().ref('games/' + gameId).update({t1Score: admin.database.ServerValue.increment(1)});
+					console.log(point);
+					
+					console.log("adding point")
+					if(point){
+						console.log(point.id);
+						if (point.type <1){
+							admin.database().ref('users/' + point.id).update({points: admin.database.ServerValue.increment(1)});
+						}
+						else{
+							admin.database().ref('users/' + point.id).update({plunks: admin.database.ServerValue.increment(1)});
+						}
+						
+						var game = await admin.database().ref('/games/' + context.params.gameId).once('value');
+							
+						
+						
+						if(point.id === game.val().player1 || point.id === game.val().player2){
+							admin.database().ref('games/' + context.params.gameId).update({t1Score: admin.database.ServerValue.increment(1)});
+						}
+						else{
+							admin.database().ref('games/' + context.params.gameId).update({t2Score: admin.database.ServerValue.increment(1)});
+						}
 					}
-					else{
-						admin.database().ref('games/' + gameId).update({t2Score: admin.database.ServerValue.increment(1)});
-					}
-					console.log(point.id);
+					
 		
 
 									
-				});
-			}
-		
-		});
+				
+			
 		
 		
 		
-	});
+		
+		
+});
     
 
 							
 		
 	
-});
